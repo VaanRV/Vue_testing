6e41421c6ec332f758bbb79d79801618a93b22bb
Francisco Duran <franciscoalanx@gmail.com>
2022-03-05 15:09:42 -0300

    [NEW] Search bar with options components

--- src/components/SelectOptions.vue
+++ src/components/SelectOptions.vue
@@ -1,18 +1,39 @@
 <template>
-  <select class="select-option" v-model="typeSearch">
-    <option disabled value="">Selecciona la busqueda</option>
-    <option v-for="option in options" :value="option.value" :key="option.value" @click="sendType(option.value)">
-      {{ option.text }}
-    </option>
-  </select>
+  <div class="search-bar-section">
+    <input type="text" class="search-bar" v-model="searchText" placeholder="Buscador" @keyup.enter="filterList()" />
+    <img class="search-image" alt="First page" src="../assets/search.png" width="25" height="25" v-on:click="filterList()" />
+    <select class="select-option" v-model="typeSearch">
+      <option disabled value="">Selecciona la busqueda</option>
+      <option v-for="option in options" :value="option.value" :key="option.value">
+        {{ option.text }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'SelectOptions',
-  props: [ 'typeSearch' ],
+  props: [ 'apiData', 'portsData', 'totalResults', 'toPage', 'fromPage', 'totalData' ],
+  updated(){
+    this.apiDataMutate = this.apiData
+    this.portsDataMutate = this.portsData
+    this.toPageMutate = this.toPage
+    this.fromPageMutate = this.fromPage
+    this.totalResultsMutate = this.totalResult
+    this.totalDataMutate = this.totalData
+  },
   data(){
       return {
+          searchText: '',
+          typeSearch: 'name',
+          orderArray: [],
+          apiDataMutate : [],
+          portsDataMutate: [],
+          toPageMutate: null,
+          fromPageMutate: null,
+          totalResultsMutate: null,
+          totalDataMutate: null,
           options: [
             { text: 'Id', value: 'id' },
             { text: 'Nombre', value: 'name' },
@@ -23,9 +44,33 @@ export default {
       }
   },
   methods: {
-    sendType(value) {
-      this.$emit('returnType', value)
-    }
+    filterList() {
+      this.orderArray = []
+      if(this.searchText.length > 0) {
+        if(this.typeSearch !== 'id' && this.typeSearch !== 'coordinates') {
+          this.apiDataMutate.filter(port => {
+            port[this.typeSearch].toLowerCase().includes(this.searchText.toLowerCase()) && this.orderArray.push(port)
+          })
+        }
+        else {
+          this.apiDataMutate.filter(port => {
+            if(port[this.typeSearch] !== null) {
+              port[this.typeSearch].toString().includes(this.searchText) && this.orderArray.push(port)
+            }
+          })
+        }
+        this.portsDataMutate = this.orderArray
+        this.totalResultsMutate = this.toPageMutate = this.portsDataMutate.length
+        this.fromPageMutate = this.portsDataMutate.length > 0 ? 1 : 0
+      }
+      else {
+        this.portsDataMutate = this.apiDataMutate
+        this.totalResultsMutate = this.totalDataMutate
+        this.toPageMutate = this.portsDataMutate.length
+        this.fromPageMutate = 1
+      }
+      this.$emit('searchRecieve', this.portsDataMutate, this.toPageMutate, this.fromPageMutate, this.totalResultsMutate)
+    },
   },
 }
 </script>
@@ -42,6 +87,41 @@ export default {
     padding-left: 10px;
   }
   select {
+    cursor: pointer;
+  }
+  .search-section {
+    display: flex;
+    align-items: center;
+  }
+  .search-bar-section {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 720px;
+    height: 45px;
+    margin: 10px 0;
+    background: #f1f3f4;
+    border: 1px solid transparent;
+    border-radius: 8px;
+    position: relative;
+  }
+  .search-bar {
+    width: 100%;
+    background: transparent;
+    border: none;
+    padding-left: 40px;
+    height: 45px;
+    outline: none;
+  }
+  .search-bar:focus {
+    background: white;
+    box-shadow: 1px 1px 1px 1px rgba(134,141,155,0.2);
+    border-radius: 8px;
+  }
+  .search-image {
+    position: absolute;
+    left: 0;
+    padding-left: 10px;
     cursor: pointer;
   }
 </style>
--- src/components/TableApi.vue
+++ src/components/TableApi.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="table-component">
     <div class="search-section">
-      <div class="search-bar-section">
-        <input type="text" class="search-bar" v-model="searchText" placeholder="Buscador" @keyup.enter="filterList(typeSearch)" />
-        <img class="search-image" alt="First page" src="../assets/search.png" width="25" height="25" v-on:click="filterList(typeSearch)" />
-        <SelectOptions :typeSearch="typeSearch" @returnType="recibeType" />
-      </div>
+      <SelectOptions :apiData="apiData" :portsData="portsData" :totalResults="totalResults" :toPage="toPage" :fromPage="fromPage" :totalData="totalData" @searchRecieve="searchPort" />
     </div>
     <Pagination :page="page" :changePage="changePage" :fromPage="fromPage" :toPage="toPage" :totalResults="totalResults" :lastPage="lastPage"  />
     <table>
@@ -61,10 +57,8 @@ export default {
   data(){
       return {
           apiData: [],
-          searchText: '',
           portsData: [],
           orderArray: [],
-          typeSearch: 'name',
           page: 1,
           lastPage: 25,
           fromPage: 0,
@@ -82,7 +76,6 @@ export default {
       const params = {
         page: this.page
       }
-
       axios
         .get('http://apitest.cargofive.com/api/ports?page=', { params })
           .then(response => {
@@ -112,35 +105,11 @@ export default {
       this.portsData = this.portsData.sort((prev, next) => this.orderArray[prev[orderName]] - this.orderArray[next[orderName]])
       this.typeOrder = true
     },
-    filterList(searchTerm) {
-      console.log(searchTerm)
-      this.orderArray = []
-      if(this.searchText.length > 0) {
-        if(searchTerm !== 'id' && searchTerm !== 'coordinates') {
-          this.apiData.filter(port => {
-            port[searchTerm].toLowerCase().includes(this.searchText.toLowerCase()) && this.orderArray.push(port)
-          })
-        }
-        else {
-          this.apiData.filter(port => {
-            if(port[searchTerm] !== null) {
-              port[searchTerm].toString().includes(this.searchText) && this.orderArray.push(port)
-            }
-          })
-        }
-        this.portsData = this.orderArray
-        this.totalResults = this.toPage = this.portsData.length
-        this.fromPage = this.portsData.length > 0 ? 1 : 0
-      }
-      else {
-        this.portsData = this.apiData
-        this.totalResults = this.totalData
-        this.toPage = this.portsData.length
-        this.fromPage = 1
-      }
-    },
-    recibeType(value){
-      this.typeSearch = value
+    searchPort(portsData, toPage, fromPage, totalResults){
+      this.portsData = portsData
+      this.totalResults = totalResults
+      this.toPage = toPage
+      this.fromPage = fromPage
     }
   },
 }
@@ -164,50 +133,5 @@ export default {
   .table-title {
     display: flex;
     justify-content: center;
-  }
-  .search-section {
-    display: flex;
-    align-items: center;
-  }
-  .search-bar-section {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 720px;
-    height: 45px;
-    margin: 10px 0;
-    background: #f1f3f4;
-    border: 1px solid transparent;
-    border-radius: 8px;
-    position: relative;
-  }
-  .search-bar {
-    width: 100%;
-    background: transparent;
-    border: none;
-    padding-left: 40px;
-    height: 45px;
-    outline: none;
-  }
-  .search-bar:focus {
-    background: white;
-    box-shadow: 1px 1px 1px 1px rgba(134,141,155,0.2);
-    border-radius: 8px;
-  }
-  .search-image {
-    position: absolute;
-    left: 0;
-    padding-left: 10px;
-    cursor: pointer;
-  }
-  .select-option {
-    position: absolute;
-    right: 0;
-    background: transparent;
-    border: unset;
-    border-left: 1px solid black;
-    height: 75%;
-    margin-right: 5px;
-    padding-left: 10px;
   }
 </style>
